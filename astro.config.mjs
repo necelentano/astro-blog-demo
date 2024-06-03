8127f832765cf3b2702090396a8093a9311d36d1
necelentano <necelentano@gmail.com>
2024-06-03 22:25:22 +0300

    #14: Build a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [icon()],
+  site: "https://astro4-demo-blog.netlify.app",
+  integrations: [icon(), sitemap()],
 });
